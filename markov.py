0fe14ae892a14beb9976a3da6176ae6af91d0b77
elijahanderson <elijahanderson_2019@depauw.edu>
2017-07-20 18:34:07 -0400

    Improved language
    - program now chooses one trend to focus on instead of all of them
    
    - checks if the message is in English and has correct grammar
    
    - fixed loop that occurs when non-English word only has one value
    
    - made code prettier

--- markov.py
+++ markov.py
@@ -5,8 +5,9 @@
 
 import tweepy
 import time
-import os
 import random
+import langid
+import language_check
 
 # Four basic steps to the program
 # 1) Read the source text
@@ -14,79 +15,77 @@ import random
 # 3) Use the Markov Chain to generate a random phrase
 # 4) Output the phrase
 
-## TODO -- 1) search only for hashtags in English
-
 # To login to my bot's twitter account
 
+
 class MyStreamListener(tweepy.StreamListener):
+
     # override tweepy.StreamListener to add logic to on_status
     def on_status(self, status):
+
         print(status.text)
 
         # Emojis can't be encoded into .txt file, so only accept tweets without them
         try :
-            status.text.encode('utf-8','strict')
+            status.text.encode('utf-8', 'strict')
             file = open('tweets.txt', 'a')
             file.write(status.text + '\n')
+
         except UnicodeError :
-            print('Tweet contained non UTF-8 chars; deleted.')
+            print('Tweet contained non UTF-8 chars; discarded.')
 
 
-
+# To "log in" to bot's twitter account
 
 
 def authenticate_twitter() :
     print('Authenticating twitter account...')
 
-<<<<<<< HEAD
-    
-=======
-    consumer_key = 'is secret shh'
-    consumer_secret = 'is secret shh'
-    access_token = 'is secret shh'
-    access_token_secret = 'is secret shh'
->>>>>>> 8c96cf800450c6f4662c1076b011a6f07e18ff0f
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
     twitter = tweepy.API(auth)
 
-    print('Authenticated as xbilboxswaggins')
+    print('Authenticated.')
 
     return twitter
 
+
 # To generate the Markov chain
 
+
 def generate_chain(text, chain={}) :
+
     words = text.split(' ')
 
     # Index should be 1, because the first word will be the key, and the subsequent word (at index 1) will be the value
     # You'll see what I mean...
     index = 1
 
-    for word in words[index:] :
+    for word_value in words[index:] :
 
         # The first word is a key
         key = words[index-1]
 
         # If a word's key (the previous word) is already in the chain, make the current word the key's value
         if key in chain :
-            chain[key].append(word)
+            chain[key].append(word_value)
+
         else :
-            chain[key] = [word]
+            chain[key] = [word_value]
 
         index += 1
 
     return chain
 
+
 # To generate the message
 # Character limit of twitter is 140
 
 
-def generate_message(chain, trendsNames) :
+def generate_message(chain, chosen_trend) :
 
-    i = random.randint(0, len(trendsNames))
-    char_limit = 140 - (len(trendsNames[i])+5)
+    char_limit = 140 - (len(chosen_trend)+5)
 
     # The first word will be a random key from the Markov chain
     word1 = random.choice(list(chain.keys()))
@@ -94,62 +93,113 @@ def generate_message(chain, trendsNames) :
     # The message will initially be just the chosen word, but capitalized
     message = word1.capitalize()
 
+    # while the length of message doesn't exceed 140 chars, keep adding words that follow the Markov chain
     while len(message) < char_limit :
         word2 = random.choice(chain[word1])
-        word1 = word2
-        message += ' ' + word2
 
-    # add a random trending hashtag to the end of the tweet
-    message += ' ' + trendsNames[i]
+        # check if the words are English
+        if is_english(word2) :
+            word1 = word2
+            message += ' ' + word2
+
+        else :
+            # if is_english() keeps returning false because a non-English word only has one value,
+            # assign a different key to word1
+            word1 = random.choice(list(chain.keys()))
+
+        print(message)
+
+    # add the chosen trend to the end of the tweet
+    message += ' ' + chosen_trend
+
+    # Fix any grammatical errors if possible (Also occasionally fixes some foreign words)
+    tool = language_check.LanguageTool('en-US')
+    matches = tool.check(message)
+    message = language_check.correct(message, matches)
+    print('Grammarized message: ' + message)
 
     # Sometimes, an emoji in twitter isn't detected by the emoji filter when the tweets all first compile.
-    # It appears in the message as '&amp'... so let's take care of it
-    print(len(message))
-    if '&amp' in message or len(message) > 140:
-        generate_message(chain, trendsNames)
+    # It appears in the message as '&amp'... so call generate_message() again until there isn't one in there
+    print('Message length: ' + len(message))
 
-    return message
+    if '&amp' in message or len(message) > 140:
+        return generate_message(chain, chosen_trend)
+
+    else :
+        return message
+
+
+# To test if the trends are in ASCII-only chars (filters out some foreign trends)
+
+
+def is_ascii(trend) :
+    return all(ord(c) < 128 for c in trend)
+
+
+# To test if the words in the final message are in English
+
+
+def is_english(word) :
+
+    print('Checking language of \'' + word + '\'...')
+    print(langid.classify(word))
+
+    if langid.classify(word)[0] == 'en':
+        print('English!')
+        return True
+
+    else:
+        print('Not English...')
+        return False
 
 
 def run_bot(twitter) :
+
+    # Clear any tweets that have been stored in the past
     open('tweets.txt', 'w').close()
 
     # Get the current trending topics from twitter
-
+    #
     # trends_place() returns a json object, but tweepy deserializes it for us. trends will be an ordinary Python list
     trends1 = twitter.trends_place(1)
 
-    # trends is a list with only one element, a dict which will be put in trends_dict
+    # trends1 is a list with only one element, a dict which will be put in trends_dict
     trends_dict = trends1[0]
 
     # grab the trends
     trends = trends_dict['trends']
 
-    #trendsNames = []
     # grab the name of each trend (only hashtags, not people)
-    trendsNames = [trend['name'] for trend in trends if trend['name'][0] == '#']
-    #for trend in trends :
-        #if trend['name'][0] == '#' :
-            #try :
-                #trendsNames.append(trend['name'].encode('ascii', 'strict'))
-            #except UnicodeError :
-                #print('Hashtag is not in English; discarded.')
+    names = [trend['name'] for trend in trends if trend['name'][0] == '#' and is_ascii(trend['name'])]
 
-    print(trendsNames)
-    # Go through a large amount of tweets from all users using a stream listener
+    print('\nThe trending hashtags: \n')
+    print(names)
 
-    # ---------- STREAMING METHOD ------------ #
+    # Go through a large amount of tweets from all users using a stream listener ######################
+    #                                                                                                 #
+    # ----------------------------------- STREAMING METHOD ------------------------------------------ #
+    #                                                                                                 #
+    # although there are several trending hashtags, the bot will choose one and use all of the tweets #
+    # that include it                                                                                 #
+    ###################################################################################################
+
+    # get a random trending hashtag
+    i = random.randint(0, len(names) - 1)
+    chosen_trend = names[i]
+
+    print('\n***************\n\nThe bot will imitate tweets concerning ' + chosen_trend + '\n\n***************\n')
 
     my_stream_listener = MyStreamListener()
     my_stream = tweepy.Stream(auth = twitter.auth, listener = my_stream_listener)
 
     # streams all tweets with trending hashtags send out by U.S. accounts
-    my_stream.filter(track=trendsNames, async=True, locations=[-169.90, 52.72, -130.53, 72.40,
-                                                                        -160.6, 18.7, -154.5, 22.3,
-                                                                        -124.90, 23.92, -66.37, 50.08])
+    my_stream.filter(track=chosen_trend, async=True, locations=[-169.90, 52.72, -130.53, 72.40,
+                                                         -160.6, 18.7, -154.5, 22.3,
+                                                         -124.90, 23.92, -66.37, 50.08])
 
     # However many seconds the program sleeps will determine how long the stream continues for
-    time.sleep(60)
+    # I use five minutes to get a TON of tweets, so I can build a good chain
+    time.sleep(60*5)
     my_stream.disconnect()
 
     tweets = ''
@@ -157,24 +207,20 @@ def run_bot(twitter) :
     with open('tweets.txt', 'r') as file :
         tweets = file.read()
 
-    print(tweets)
-
     # Generate the Markov chain
     chain = generate_chain(tweets)
 
-
     # Generate the message using the chain
-    message = generate_message(chain, trendsNames)
+    message = generate_message(chain, chosen_trend)
 
     print('The Markov chain: ' + message)
 
     # Tweet out the resulting Markov chain!
-
     twitter.update_status(message)
 
-    print('Sleeping for one hour...')
-    # Bot will tweet once every hour (3,600 seconds)
-    time.sleep(60*20)
+    print('Sleeping for about thirty minutes...')
+    # Bot will tweet once every 30 minutes or so while running
+    time.sleep(60*25)
 
 twitter = authenticate_twitter()
 
